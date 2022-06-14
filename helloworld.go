62c8afa7a961d9bf3254467e1c41de88c56a2355
ofuji-works <ofuji.works@gmail.com>
2022-06-15 00:22:08 +0900

    四則演算

--- helloworld.go
+++ helloworld.go
@@ -16,4 +16,20 @@ func main () {
 	} else {
 		fmt.Println(false)
 	}
+
+	// 四則演算
+	add := 1 + 2
+	fmt.Println(add)
+
+	substract := 3 - 1
+	fmt.Println(substract)
+
+	multiplication := 5 * 5
+	fmt.Println(multiplication)
+
+	division := 25 / 5
+	fmt.Println(division)
+
+	rest := 26 % 5
+	fmt.Println(rest)
 }
